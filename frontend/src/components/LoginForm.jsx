d08a397c6038e192e1006f90aaa9a95f21c03cd0
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-30 13:08:12 +0100

    Imported props to use them in component's elements and display loader on button with conditionnal rendering

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { IoEye } from 'react-icons/io5';
 import { RiLoginCircleLine } from 'react-icons/ri';
 
+import loader from './../assets/loader.svg';
 import './LoginForm.scss';
 
-const LoginForm = () => {
+const LoginForm = ({
+  isLoading,
+  handleOnChange,
+  handleOnBlur,
+  handleSubmit,
+}) => {
+
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false);
+
   return (
     <>
-      <form action="POST" className="login-form">
+      <form action="POST" className="login-form" onSubmit={handleSubmit}>
         <h1 className="login-form__title">Login</h1>
         <input
           type="text"
           name="email"
           className="login-form__input-text"
           placeholder="email@email.com"
-          autofocus
+          autoFocus
+          onChange={handleOnChange}
+          onBlur={handleOnBlur}
         />
 
         <div className="login-form__password-container">
@@ -25,12 +37,18 @@ const LoginForm = () => {
             name="password"
             className="login-form__input-password"
             placeholder="password"
+            onChange={handleOnChange}
+            onBlur={handleOnBlur}
           />
           <IoEye className="login-form__eye-icon" />
         </div>
 
         <button className="login-form__submit-btn">
-          <RiLoginCircleLine className="login-form__btn-icon" />
+          {isLoading ? (
+            <img src={loader} alt="loader" />
+          ) : (
+            <RiLoginCircleLine className="login-form__btn-icon" />
+          )}
         </button>
 
         <div className="form-links">
